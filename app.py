fd0dde6201a26332339cdbcfba42d0e7f3126900
tensey <tensey26@gmail.com>
2025-05-08 13:15:48 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'video' not in request.files:
+        return redirect(request.url)
+    file = request.files['video']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(path)
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
